5615b8403bc5d6b2296623bd3e1d4cdd78d81f9a
Adam Burmister <adam.burmister@gmail.com>
2016-06-25 15:30:35 -0700

    make more generic

--- bin/cron.sh
+++ bin/cron.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT_PATH="/usr/local/src/hype_machine/bin/"
+IP_ADDR=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+HYPEM_USERNAME='flog'
 
 echo "Clearing old files"
 rm -rf /tmp/hypem/*
@@ -14,12 +16,12 @@ echo "Generating Hype Machine Popular tracks M3U playlist"
 for f in /tmp/hypem/popular/*.mp3; do echo "$f" >> /tmp/hypem/popular.m3u; done
 
 echo "Downloading Hype Machine Favourite tracks"
-`${SCRIPT_PATH}/hypem -w 10 flog /tmp/hypem/favorites`
+`${SCRIPT_PATH}/hypem -w 10 $HYPEM_USERNAME /tmp/hypem/favorites`
 
 echo "Generating Hype Machine Favourite tracks M3U playlist"
 for f in /tmp/hypem/favorites/*.mp3; do echo "$f" >> /tmp/hypem/favorites.m3u; done
 
 echo "Setting playlist to point smb://"
-sed -i 's/tmp/smb:\/\/192.168.1.110/g' /tmp/hypem/*.m3u
+`sed -i 's/tmp/smb:\/\/$IP_ADDR/g' /tmp/hypem/*.m3u`
 
 echo "Done!"
